d5a585908b0720a1661602b3357f6088f9329b2e
vito <sinclair.shu@immotor.com>
2019-06-13 17:02:14 +0800

    投保人信息

--- source/policy-holder/src/api/api.js
+++ source/policy-holder/src/api/api.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 
 //测试环境
-const ehdServerPath = 'https://test.ehuandian.net';
+const ehdServerPath = 'https://test.ehuandian.net/';
 var yApi = {
 	//查询已填保单信息
 	getInsurancePolicyInfo: ehdServerPath + 'insurance/user/details/get',
+	//修改投保人信息
+	addInsurancePolicyInfo: ehdServerPath + 'insurance/user/details/add',
 };
 
 export default yApi;
--- source/policy-holder/src/components/policyholder.vue
+++ source/policy-holder/src/components/policyholder.vue
@@ -5,93 +5,70 @@
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">姓名</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.name}}</em>
+            <input v-bind:style="{width:inputWidth}" type="text" maxlength="20" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.name"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">身份证号码</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.idcard}}</em>
+            <input v-bind:style="{width:inputWidth}" type="text" maxlength="18" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.idcard"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">手机号码</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.phone}}</em>
+            <input v-bind:style="{width:inputWidth}" type="tel" maxlength="11" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.phone"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">车辆SN码</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.prechar2}}</em>
+            <input v-bind:style="{width:inputWidth}" type="text" maxlength="40" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.prechar2"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">电机号</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.makeFactory}}</em>
+            <input v-bind:style="{width:inputWidth}" type="text" maxlength="50" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.makeFactory"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">车架号</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.fuelName}}</em>
+            <input v-bind:style="{width:inputWidth}" type="text" maxlength="20" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.fuelName"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">地址</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.address}}</em>
+            <input v-bind:style="{width:inputWidth}" type="text" maxlength="50" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.address"></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
         <li>
           <div class="holderULlidiv">
             <em class="inputTitleClass textFontClass">邮箱</em>
-            <em
-              v-bind:style="{width:inputWidth}"
-              type="text"
-              class="inputTextClass textFontClass"
-            >{{policy_holderData.email}}</em>
+            <input v-bind:style="{width:inputWidth}" type="email" maxlength="20" autocomplete="off" class="inputTextClass textFontClass"
+						 v-model="policy_holderData.email" placeholder=""></input>
             <div class="holderLineClass"></div>
           </div>
         </li>
       </ul>
     </div>
+    <div class="submitButton" @click="savePolicyHolderData()">保存</div>
   </div>
 </template>
 
@@ -103,10 +80,30 @@ export default {
       headerImgHeight: (screen.width * 110) / 375 + 'px',
       inputWidth: screen.width - 130 + 'px',
       policy_holderData: null,
-      userToken: ''
+      userToken: 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjEzNTM0MDQwMzU5IiwidUlEIjo0NDk1LCJ0aW1lIjoxNTYwNDEzMTM5ODU0fQ.IvHLqlxszLiCde1XyGAIWyirWxu0ODzOPfUEW6zDOT0'
     }
   },
   methods: {
+    // 是不是手机号码
+    isPhoneNumber: function (val) {
+      var reg = /^1[0-9]{10}$/
+      return reg.test(val)
+    },
+    isIDCardNumber: function (val) {
+      var idcardReg =
+					/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/
+      return idcardReg.test(val)
+    },
+    isEmail: function (val) {
+      var reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/
+      return reg.test(val)
+    },
+    isEmptyStr: function (val) {
+      if (val && val.length > 0) {
+        return false
+      }
+      return true
+    },
     fetchPolicyHolderData: function () {
       var vueThis = this
       vueThis
@@ -131,6 +128,72 @@ export default {
           window.location.href =
             'IMMOTOR://showPrompt?code=0&message=网络连接似乎已断开，请检查您的网络设置'
         })
+    },
+    savePolicyHolderData: function () {
+      var vueThis = this
+
+      if (vueThis.isEmptyStr(vueThis.policy_holderData.name)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入姓名'
+        return
+      }
+      if (!vueThis.isIDCardNumber(vueThis.policy_holderData.idcard)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入正确的身份证号'
+        return
+      }
+      if (!vueThis.isPhoneNumber(vueThis.policy_holderData.phone)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入正确的手机号'
+        return
+      }
+      if (vueThis.isEmptyStr(vueThis.policy_holderData.prechar2)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入你绑定的车辆SN码'
+        return
+      }
+      if (vueThis.isEmptyStr(vueThis.policy_holderData.makeFactory)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入电机号'
+        return
+      }
+      if (vueThis.isEmptyStr(vueThis.policy_holderData.fuelName) || vueThis.policy_holderData.fuelName.length > 20) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入正确的车架号'
+        return
+      }
+      if (vueThis.isEmptyStr(vueThis.policy_holderData.address)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入地址'
+        return
+      }
+      if (!vueThis.isEmail(vueThis.policy_holderData.email)) {
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=请输入正确的邮箱'
+        return
+      }
+      vueThis.axios({
+        method: 'post',
+        url: vueThis.$yApi.addInsurancePolicyInfo,
+        data: {
+          'name': vueThis.policy_holderData.name,
+          'idcard': vueThis.policy_holderData.idcard,
+          'email': vueThis.policy_holderData.email,
+          'address': vueThis.policy_holderData.address,
+          'phone': vueThis.policy_holderData.phone,
+          'prechar2': vueThis.policy_holderData.prechar2,
+          'makeFactory': vueThis.policy_holderData.makeFactory,
+          'fuelName': vueThis.policy_holderData.fuelName
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': vueThis.userToken
+        }
+      }).then(function (resp) {
+        var data = resp.data
+        if (data.code === 600) {
+          window.location.href = 'IMMOTOR://showPrompt?code=1&message=修改保单信息成功'
+          window.location.href = 'IMMOTOR://addPolicyInfoSuccess'
+        } else {
+          var messageStr = data.msg
+          window.location.href = 'IMMOTOR://showPrompt?code=0&message=' + messageStr
+        }
+      }).catch(resp => {
+        console.log('请求失败：' + resp.status + ',' + resp.statusText)
+        window.location.href = 'IMMOTOR://showPrompt?code=0&message=网络连接似乎已断开，请检查您的网络设置'
+      })
     }
   },
   mounted: function () {
@@ -284,4 +347,20 @@ export default {
   position: absolute;
   background-color: #e5e5e5;
 }
+
+.submitButton {
+  position: absolute;
+  left: 10px;
+  right: 10px;
+  bottom: 40px;
+  height: 50px;
+  background: rgba(252, 145, 83, 1);
+  border-radius: 2px;
+  font-size: 16px;
+  font-family: PingFangSC-Regular;
+  font-weight: 400;
+  color: rgba(255, 255, 255, 1);
+  line-height: 50px;
+}
+
 </style>
