225f08ae32620555fd2eadfdf6f71279a7230b6c
vito <sinclair.shu@immotor.com>
2019-06-21 19:02:31 +0800

    填写保单页面 + 投保人信息 + 调查问卷页面正式环境

--- source/policy-holder/src/api/api.js
+++ source/policy-holder/src/api/api.js
@@ -1,7 +1,10 @@
 /* eslint-disable */
 
 //测试环境
-const ehdServerPath = 'https://test.ehuandian.net/';
+// const ehdServerPath = 'https://test.ehuandian.net/';
+//正式环境
+const ehdServerPath = 'https://ehd.ehuandian.net/';
+
 var yApi = {
 	//查询已填保单信息
 	getInsurancePolicyInfo: ehdServerPath + 'insurance/user/details/get',
