cf933843559ce00e17b16427b82969935e639b3c
vito <sinclair.shu@immotor.com>
2019-09-20 15:59:25 +0800

    换电异常指南 公告

---
+++ source/swapExceptionCourse/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Course/>
+  </div>
+</template>
+
+<script>
+import Course from './components/Course'
+
+export default {
+  name: 'App',
+  components: {
+    Course
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100%;
+}
+</style>
---
+++ source/swapExceptionCourse/src/components/Course.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <video-player
+      class="video-player vjs-custom-skin"
+      ref="videoPlayer"
+      :playsinline="true"
+      :options="playerOptions"
+    ></video-player>
+    <!-- <videoPlayer
+      class="video-player vjs-custom-skin"
+      ref="videoPlayer"
+      :playsinline="true"
+      :options="playerOptions"
+    ></videoPlayer> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Course",
+  data() {
+    return {
+      playerOptions: {
+        autoplay: false,
+        muted: false,
+        loop: false,
+        preload: "auto",
+        language: "zh-CN",
+        aspectRatio: "9:16",
+        fluid: true,
+        sources: [
+          {
+            type: "video/mp4",
+            src: "https://imgcn.immotor.com/promotion/video/tutorial.mp4"
+          }
+        ],
+        poster: require("../assets/video.png"),
+        width: document.documentElement.clientWidth,
+        notSupportedMessage: "此视频暂无法播放，请稍后再试",
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: false
+        }
+      }
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
